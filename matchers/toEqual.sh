af77af7da9abcade43e5973a7317544cc1bb15ac
Rebecca Taylor <bex@beccasaur.us>
2020-07-27 13:02:52 -0700

    Still tweaking toEqual with cat -A

--- matchers/toEqual.sh
+++ matchers/toEqual.sh
@@ -1,9 +1,9 @@
 expect.matcher.toEqual() {
   local negateResults="$1"; shift
-  local actualResult="$1";  shift
-  local expectedResult="$@"
-  actualResult="$( echo "$actualResult" | cat -A )"
-  expectedResult="$( echo "$*" | cat -A )"
+  local actualResult="$1"; shift
+  local expectedResult="$1"
+  actualResult="$( echo -e "$actualResult" | cat -A )"
+  expectedResult="$( echo -e "$expectedResult" | cat -A )"
   if [ "$actualResult" = "$expectedResult" ]
   then
     [ "$negateResults" = "true" ] && expect._failureMessage "$negateResults" equal "'$actualResult'" "'$expectedResult'"

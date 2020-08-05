70c9de45756b499598bc3a2fef7f67b8f37b8e6b
Rebecca Taylor <bex@beccasaur.us>
2020-08-04 22:49:02 -0700

    BASH 3.2 tests

--- package.sh
+++ package.sh
@@ -2,6 +2,11 @@ name expect
 
 description "🧐 Expectations"
 
+script shell    bx multi-bash run 3.2
+script test     bx multi-bash build-and-run 3.2,latest ./packages/bin/spec
+script test-3.2 bx multi-bash build-and-run 3.2        ./packages/bin/spec
+
+
 devDependency spec
 devDependency assert
 devDependency run-command

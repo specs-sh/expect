bdd32e333ef6dc67fc1472e675004cd1e9db1094
Rebecca Taylor <bex@beccasaur.us>
2021-05-11 16:45:34 -0700

    expect array myItems not to equal A B C

--- expect.sh
+++ expect.sh
@@ -1,7 +1,8 @@
 set -uo pipefail # Remove when ready for production
 
 expect() {
-  local EXPECT_ACTUAL= EXPECT_MATCHER= EXPECT_NOT=
+  local EXPECT_ACTUAL= EXPECT_MATCHER= EXPECT_NOT= \
+    EXPECT_BASH_ASSOCIATIVE_ARRAYS= EXPECT_BASH_NAME_REFERENCES=
 
   # Block handling...
 
@@ -10,14 +11,22 @@ expect() {
 
   # (( $# == 0 )) && echo TODO ERROR
 
-  EXPECT_MATCHER="${EXPECT_FUNCTION_PREFIX:-expect.}$1"; shift
+  EXPECT_MATCHER="${EXPECT_FUNCTION_PREFIX:-expect.matcher.}$1"; shift
+
+  (( ${BASH_VERSINFO[0]} >= 4 )) && EXPECT_BASH_ASSOCIATIVE_ARRAYS=true
+  (( ${BASH_VERSINFO[0]} >= 5 )) || { (( ${BASH_VERSINFO[0]} == 4 )) && (( ${BASH_VERSINFO[1]} >= 3 )); } && EXPECT_BASH_NAME_REFERENCES=true
 
   while (( $# > 0 )) && ! declare -F "$EXPECT_MATCHER" &>/dev/null; do
-    EXPECT_MATCHER+=".$1"; shift
+    if [ "$1" = not ]; then
+      EXPECT_NOT=true; shift
+    else
+      EXPECT_MATCHER+=".$1"; shift
+    fi
   done
 
+  # TODO support .and fluent
+
   if declare -F "$EXPECT_MATCHER" &>/dev/null; then
-    echo "MATCHER $EXPECT_MATCHER" >&2
     "$EXPECT_MATCHER" "$@"
   else
       echo "ERROR NO MATCHER TODO" >&2
@@ -26,5 +35,13 @@ expect() {
 }
 
 expect.inspect() {
-  echo -e "$1" | cat -vET
+  printf "'%s'" "$( echo -ne "$1" | cat -vET )"
+}
+
+expect.inspectList() {
+  while (( $# > 0 )); do
+    expect.inspect "$1"
+    shift
+    (( $# > 0 )) && printf ' '
+  done
 }
--- matchers/equals.sh
+++ matchers/equals.sh
@@ -1,19 +1,15 @@
-expect.to.equal() {
-  local expected="$1"
-  if [ "$EXPECT_NOT" = true ]; then
-    if [ "$EXPECT_ACTUAL" = "$expected" ]; then
-      printf "Expected results not to equal:\nActual: '%s'\nExpected: '%s'\n" \
-        "$( echo -e "$expected" | cat -vET )" \
-        "$( echo -e "$EXPECT_ACTUAL" | cat -vET )" >&2
-      return 1
-    fi
-  else
-    if [ "$EXPECT_ACTUAL" != "$expected" ]; then
-      printf "Expected results to equal:\nActual: '%s'\nExpected: '%s'\n" \
-        "$( echo -e "$expected" | cat -vET )" \
-        "$( echo -e "$EXPECT_ACTUAL" | cat -vET )" >&2
-      return 1
-    fi
+expect.matcher.to.equal() {
+  local __expected__="$1"
+  if [ "$EXPECT_NOT" = true ] && [ "$EXPECT_ACTUAL" = "$__expected__" ]; then
+    printf "Expected results not to equal:\nActual: %s\nExpected: %s\n" \
+      "$(expect.inspect "$__expected__" )" \
+      "$(expect.inspect "$EXPECT_ACTUAL" )" >&2
+    return 1
+  elif [ "$EXPECT_NOT" != true ] && [ "$EXPECT_ACTUAL" != "$__expected__" ]; then
+    printf "Expected results to equal:\nActual: %s\nExpected: %s\n" \
+      "$( expect.inspect "$__expected__" )" \
+      "$( expect.inspect "$EXPECT_ACTUAL" )" >&2
+    return 1
   fi
   return 0
 }

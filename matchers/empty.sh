3cf57c5dc0a15950a4dcb3df8a0d10e3511911d6
Rebecca Taylor <bex@beccasaur.us>
2021-05-17 18:16:46 -0700

    expect items array to be empty

--- matchers/empty.sh
+++ matchers/empty.sh
@@ -4,25 +4,29 @@ ExpectMatcher.empty() {
     if [ "$EXPECT_BASH_NAME_REFERENCES" = true ]; then
       local -n __array__="$EXPECT_ACTUAL"
       if [ "$EXPECT_NOT" = true ] && (( ${#__array__[@]} == 0 )); then
-        printf "Expected array not to have zero elements\nActual: %s\n" \
-          "$( ExpectMatchers.utils.inspect "$EXPECT_ACTUAL" )" >&2
+        printf "Expected array not to have zero elements\nActual: ( )%s\n"
         return 52
       elif [ "$EXPECT_NOT" != true ] && (( ${#__array__[@]} > 0 )); then
-        printf "Expected array to have zero elements\nActual: (%s)\nExpected: ( )\n" \
-          "$( ExpectMatchers.utils.inspectList "${__array__[@]}" )" >&2
+        printf "Expected array to have zero elements\nActual: (%s)\nExpected: ( )\n" "$( ExpectMatchers.utils.inspectList "${__array__[@]}" )" >&2
         return 52
       fi
     else
-      :
+      local -i __arrayLength__=
+      eval "__arrayLength__=\"\${#EXPECT_ACTUAL}\""
+      if [ "$EXPECT_NOT" = true ] && (( __arrayLength__ == 0 )); then
+        printf "Expected array not to have zero elements\nActual: ( )%s\n"
+        return 52
+      elif [ "$EXPECT_NOT" != true ] && (( __arrayLength__ > 0 )); then
+        eval "printf \"Expected array to have zero elements\nActual: (%s)\nExpected: ( )\n\" \"\$( ExpectMatchers.utils.inspectList \"\${$EXPECT_ACTUAL[@]}\" )\"" >&2
+        return 52
+      fi
     fi
   else
     if [ "$EXPECT_NOT" = true ] && [ -z "$EXPECT_ACTUAL" ]; then
-      printf "Expected text not to have zero-length\nActual: %s\n" \
-        "$( ExpectMatchers.utils.inspect "$EXPECT_ACTUAL" )" >&2
+      printf "Expected text not to have zero-length\nActual: %s\n" "$( ExpectMatchers.utils.inspect "$EXPECT_ACTUAL" )" >&2
       return 52
     elif [ "$EXPECT_NOT" != true ] && [ -n "$EXPECT_ACTUAL" ]; then
-      printf "Expected text to have zero-length\nActual: %s\nExpected: \"\"\n" \
-        "$( ExpectMatchers.utils.inspect "$EXPECT_ACTUAL" )" >&2
+      printf "Expected text to have zero-length\nActual: %s\nExpected: \"\"\n" "$( ExpectMatchers.utils.inspect "$EXPECT_ACTUAL" )" >&2
       return 52
     fi
   fi
